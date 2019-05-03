286fa86c72bcdf7706e6532725d3722e40de23b4
johnfrank91 <johnfrank91@users.noreply.github.com>
2019-05-03 00:35:01 -0500

    Modified to almost final version of the FrontEnd

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
-  providers: [UserService]
 })
 export class RegisterComponent implements OnInit {
 
-  constructor( private userService1: UserService) { }
+  constructor( ) { }
 
   ngOnInit() {
   }
