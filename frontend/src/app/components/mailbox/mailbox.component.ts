662ed4b9b47e34df3ae7d5e990d7dc30cffb6f65
duranruizdavid <duranruizdavid@gmail.com>
2019-05-16 19:54:26 -0500

    Casi lista, solo falta cmabiar el color para el titulo de cuantos mensajes sin leer hay

--- frontend/src/app/components/mailbox/mailbox.component.ts
+++ frontend/src/app/components/mailbox/mailbox.component.ts
@@ -7,7 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MailboxComponent implements OnInit {
 
-  constructor() { }
+  dataListUnread: { "name": string; "body": string; "date": string; }[];
+  dataListRead: { "name": string; "body": string; "date": string; }[];
+  constructor() { 
+    this.dataListUnread=[{
+      "name":"Mario Casas",
+      "body":"Buenas tardes: Me pongo en contacto con usted para agrendar una visita...",
+      "date":"14/05/2019"      
+    },
+    {
+      "name":"Cesar Mazo",
+      "body":"Hola, te escribo porque me interesa el cuarto que estas anunciando...",
+      "date":"09/05/2019"      
+    },
+    {
+      "name":"Laura Guadarrama",
+      "body":"Buenos días, tengo una duda con el depa que estás ofreciendo en...",
+      "date":"09/05/2019"      
+    }];
+    this.dataListRead=[{
+      "name":"Federico Esponda",
+      "body":"Hola: Quisiera saber cuánto es lo mínimo por el cuarto en Frontera",
+      "date":"03/05/2019"      
+    },
+    {
+      "name":"Andrés Gomez",
+      "body":"el departamento de chihuahua 650 tiene baño completo? no me quedó claro",
+      "date":"01/05/2019"      
+    }];
+  }
 
   ngOnInit() {
   }
